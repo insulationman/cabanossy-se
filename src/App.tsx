1a942e89f36e6d22d717c079c55c30399ce1be1f
Adam Liljenberg <adam@cabanossy.se>
2024-05-02 17:44:52 +0200

    Total update

--- src/App.tsx
+++ src/App.tsx
@@ -1,200 +1,175 @@
 import Logo from "./assets/Cabanossy-Snurra_nobg.svg";
-import { useEffect, useRef, useState } from "react";
-import PilUpp from "./assets/Pil-Upp.svg";
-import PilNer from "./assets/Pil-Ner.svg";
-import PieChart from "./assets/PieChart.svg";
-import Card from "./Card";
-import Footer from "./Footer";
-import Logos from "./assets/logos.png";
+import Portrait from "./assets/portrait.jpg";
 
 function App() {
-  const imageRef = useRef<HTMLImageElement | null>(null);
-  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
-  const [colorNameBG, setColorNameBG] = useState("bg-blue-200");
-  const [colorNameCard, setColorNameCard] = useState("bg-cyan-100");
-
-  const BGColorNames = ["bg-amber-200", "bg-blue-200"];
-
-  const CardColorNames = ["bg-yellow-200", "bg-cyan-100"];
-
-  useEffect(() => {
-    scrollContainerRef.current?.addEventListener("scroll", () => {
-      requestAnimationFrame(() => {
-        const rotation = (scrollContainerRef.current?.scrollTop || 0) / 2;
-        if (imageRef.current != undefined) {
-          imageRef.current.style.transform = `rotate(${rotation}deg)`;
-        }
-      });
-    });
-  }, []);
-
-  const handleColorChange = () => {
-    // //set the color of the background to the next color in the array
-    const nextBGColorIndex = BGColorNames.indexOf(colorNameBG) + 1;
-    const nextBGColor = BGColorNames[nextBGColorIndex % BGColorNames.length];
-    setColorNameBG(nextBGColor);
-    //and the color of the cards
-    const nextCardColorIndex = CardColorNames.indexOf(colorNameCard) + 1;
-    const nextCardColor =
-      CardColorNames[nextCardColorIndex % CardColorNames.length];
-    setColorNameCard(nextCardColor);
-    // setColorNameBG("blue");
-  };
-
   return (
-    <div className={`flex h-[100dvh] w-full justify-center ${colorNameBG}`}>
-      <div className=" h-full flex w-full justify-center flex-col">
-        <div className=" h-[8rem] md:h-64 p-6 flex justify-center items-center top-0 shrink-0">
+    <div className="items-center ">
+      <div className="  gap-8 grid grid-cols-1 md:grid-cols-2 md:gap-8 ">
+        <div className="bg-white py-24 col-span-2 flex flex-col items-center p-3 md:p-16">
           <img
             src={Logo}
-            className={"h-24 md:h-32 rounded-full" + " " + colorNameCard}
+            className={"h-24 md:h-32 shrink rounded-full"}
             alt="logo"
-            ref={imageRef}
             style={{ filter: "drop-shadow(3px 5px 2px rgb(0 0 0 / 0.4));" }}
           />
-        </div>
-        <div
-          className="grow overflow-scroll no-scrollbar pb-auto flex items-center flex-col"
-          ref={scrollContainerRef}
-          onScroll={() => console.log("scroll")}
-        >
-          <Card title="A passion for web and coding" colorName={colorNameCard}>
-            <p>
-              At Cabanossy we are passionate – about web development and code in
-              particular. We believe that the web is a great place to be.
+          <div className="flex flex-col py-12 text-center gap-5">
+            <h1 className=" text-3xl md:text-6xl font-bold ">
+              Web och mjukvara
+            </h1>
+            <p className="text-md md:text-xl max-w-[700px]">
+              Vi brinner för webbutveckling och kod. För att webben är en
+              fantastisk plats att vara på.
             </p>
-            <img
-              src={PilNer}
-              className="h-2 mt-auto self-end overflow-visible"
-              alt="pilned"
-            />
-          </Card>
-          <Card title="Grow with IT" colorName={colorNameCard}>
-            <p>
-              This knowledge about new and established technologies helps our
-              customers achieve their goals.
-            </p>
-            <img src={PilNer} className="h-2 mt-auto self-end" alt="pilned" />
-          </Card>
-          <Card title="What we do" colorName={colorNameCard}>
-            <div className="flex flex-col md:flex-row  gap-3">
-              <p className="order-2 md:order-first">
-                We do work in three business areas: classic public facing web
-                development, customised business software and IT
-                management/consulting.
-              </p>
-              <img
-                src={PieChart}
-                className="h-24 md:h-32 md:mx-4 md:order-last self-center"
-              />
-            </div>
-            <img src={PilNer} className="h-2 mt-auto self-end" alt="pilned" />
-          </Card>
-          <Card title="Web development" colorName={colorNameCard}>
-            <p>
-              We often work with designers to create responsive sites for
-              end-customers. Here are some examples:
-            </p>
-            <ul className="list-disc list-inside">
-              <li>
-                <a
-                  href="https://viab.se"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline"
-                >
-                  Vätterledens Invest
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://supai.se/curryketchup/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline"
-                >
-                  Supai kitchen
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://vhab.se"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline"
-                >
-                  VHAB
-                </a>
-              </li>
-            </ul>
-            <img src={PilNer} className="h-2 mt-auto self-end" alt="pilned" />
-          </Card>
-          <Card title="From design to site" colorName={colorNameCard}>
-            <p>
-              In our work with marketing agency <strong>2028</strong>, this
-              means we take a design and turn it into a site while collaborating
-              closely.
-            </p>
-            <img src={PilNer} className="h-2 mt-auto self-end" alt="pilned" />
-          </Card>
-          <Card title="Custom software" colorName={colorNameCard}>
-            <p>
-              Collaboration is also key when you need a customised solution.
-              This solution was made by working closely with users, and the
-              result is a successful project:
-            </p>
-            <a
-              href="https://isol.se"
-              target="_blank"
-              rel="noreferrer"
-              className="underline"
-            >
-              isol.se
-            </a>
-            <img src={PilNer} className="h-2 mt-auto self-end" alt="pilned" />
-          </Card>
-          <Card title="Even more customized" colorName={colorNameCard}>
-            <p>
-              The Gothenburg based company IT-Trans needed tools for storing and
-              delivering printing soultions. They now use a custom delivery and
-              order management software. Contact us to learn more.
-            </p>
-            <img src={PilNer} className="h-2 mt-auto self-end" alt="pilned" />
-          </Card>
-          <Card title="Selected tools" colorName={colorNameCard}>
-            <p>
-              All this is made possible by using the right tools, some of the
-              most common being:
-              <strong>
-                {" "}
-                .NET, React, TypeScript, PostgreSQL, Docker, GitHub Actions,
-                Azure and of course WordPress.
-              </strong>
-            </p>
-            <img src={PilNer} className="h-2 mt-auto self-end" alt="pilned" />
-          </Card>
-          <Card title="Just humans" colorName={colorNameCard}>
-            <p>
-              All this digital development is usually done in the context of an
-              organisation. We help our customers make the right decisions
-              within that context.
-            </p>
-            <img src={PilNer} className="h-2 mt-auto self-end" alt="pilned" />
-          </Card>
-          <Card title="Get in touch" colorName={colorNameCard}>
-            <p>
-              Do you want to know more? Get in touch – we will get back to you
-              as soon as possible.
-            </p>
-            <img src={PilUpp} className="h-2 mt-auto self-end" alt="pilned" />
-          </Card>
-          <Card title="Selected clients" colorName={colorNameCard}>
-            <img src={Logos} alt="logos" className="w-full my-auto" />
-          </Card>
-          <Footer onColorChange={handleColorChange} />
-          <div className="min-h-[700px]">:)</div>
+          </div>
         </div>
       </div>
+      <div className="flex flex-col items-center gap-8 py-12 md:py-24 ">
+        <div className="max-w-[1300px] p-3 md:p-0">
+          <h1 className="text-2xl  md:text-4xl font-bold text-center md:text-left">
+            Våra affärsområden
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 py-12 ">
+            <div className="bg-white rounded-lg">
+              <h1 className="text-2xl font-bold px-8 py-4 bg-blue-300 rounded-t-lg ">
+                Klassisk webbutveckling
+              </h1>
+              <p className="p-8  ">
+                Detta är antagligen vad du som reklambyrå eller företag är
+                intresserad av. Vanligtvis handlar det om att bygga en hemsida
+                eller en webbapplikation för att dela med dig av ditt ett
+                varumärke eller budskap.
+              </p>
+            </div>
+            <div className="bg-white rounded-lg">
+              <h1 className="text-2xl font-bold p-8 bg-blue-300 rounded-t-lg">
+                Affärsapplikationer
+              </h1>
+              <p className="p-8">
+                Vi bygger applikationer som hjälper dig att driva ditt företag
+                framåt. Det kan vara allt från en enkel kunddatabas till en
+                komplex affärssystem.
+              </p>
+            </div>
+            <div className="bg-white rounded-lg">
+              <h1 className="text-2xl font-bold p-8 bg-blue-300 rounded-t-lg">
+                E-handel
+              </h1>
+              <p className="p-8">
+                Vi bygger e-handelslösningar som hjälper dig att sälja dina
+                produkter online. Vi kan hjälpa dig att komma igång med en enkel
+                webbshop eller en mer avancerad e-handelslösning.
+              </p>
+            </div>
+            <div className="bg-white rounded-lg">
+              <h1 className="text-2xl font-bold p-8 bg-blue-300 rounded-t-lg">
+                Företagskonsulting
+              </h1>
+              <p className="p-8">
+                Många företag har idag möjlighet att använda sig av digitala
+                verktyg för att nå sina mål. Vi erbjuder konsulttjänster för att
+                lyckas med detta.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center pb-12 md:pb-40 gap-4  p-3 md:p-0">
+        <div>
+          <div className="max-w-[1300px] min-h-96 grid grid-cols-1 md:grid-cols-2 rounded-xl bg-white">
+            <img
+              src={Portrait}
+              className="object-contain rounded-t-lg md:rounded-r-none md:rounded-l-xl"
+            />
+            <div className="p-5 md:p-8 flex flex-col gap-4">
+              <h3 className="text-4xl font-bold md:mb-2">
+                Vi håller oss uppdaterade
+              </h3>
+              <p className="md:text-xl">
+                Jag, Adam, och mina samarbetspartners håller oss ständigt
+                uppdaterade med den senaste tekniken och trenderna inom
+                webbutveckling. Vi är passionerade om att skapa webbplatser som
+                är moderna och användarvänliga.
+              </p>
+              <small>
+                <a href="https://www.linkedin.com/in/adam-liljenberg/">
+                  Läs mer om mig på LinkedIn
+                </a>
+              </small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center  pb-48 gap-4 p-3 pb-12 md:p-0 md:pb-24">
+        <h1 className="text-2xl font-bold pb-6 text-center md:text-left">
+          Vi presenterar några webbplatser:
+        </h1>
+
+        <div className="max-w-[1300px]">
+          <div className="gap-4 grid grid-cols-1">
+            <div className="bg-white rounded-lg p-8 max-w-[1000px]">
+              <h1 className="text-2xl font-bold ">Vätterledens Invest</h1>
+              <p className="mb-2">
+                Tillsammans med designers från marknadsbyrån{" "}
+                <strong>2028</strong> skapade vi en hemsida för Vätterledens
+                Invest. Vi är mycket nöjda med resultatet:
+              </p>
+              <a
+                className="bg-blue-200 p-1 px-3 rounded-lg "
+                target="_blank"
+                href="https://viab.se"
+              >
+                Besök hemsidan
+              </a>
+            </div>
+            <div className="bg-white rounded-lg p-8 max-w-[1000px]">
+              <h1 className="text-2xl font-bold ">Supai Kitchen</h1>
+              <p className="mb-2">
+                Supai beskriver sig själva som en stark kraft i göteborgs
+                matvärld och vi är glada att ha fått möjligheten arbeta
+                tillsammans.
+              </p>
+              <a
+                className="bg-blue-200 p-1 px-3 rounded-lg "
+                target="_blank"
+                href="https://supai.se/curryketchup/"
+              >
+                Kolla in deras curryketchup
+              </a>
+            </div>
+            <div className="bg-white rounded-lg p-8 max-w-[1000px]">
+              <h1 className="text-2xl font-bold ">VHAB</h1>
+              <p className="mb-2">
+                VHAB förvaltar och investerar i bland annat företag och
+                fastigheter. Vi har tillsammans med webb-designers hjälpt dem
+                att skapa en hemsida som speglar deras verksamhet.
+              </p>
+              <a
+                className="bg-blue-200 p-1 px-3 rounded-lg "
+                target="_blank"
+                href="https://supai.se/curryketchup/"
+              >
+                Ta en titt
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="flex flex-col items-center py-20 gap-4 bg-white">
+        <div className="max-w-[1300px] grid grid-cols-3">
+          <div>
+            <h3 className="text-4xl font-bold mb-2">
+              isol.se - Webverktyg för sveriges isolerare
+            </h3>
+            <p>
+              Tillsammans med isoleringsföretag har vi utvecklat en tjänst för
+              att underlätta deras arbete. Företagen som använder sig av
+              tjänsten kan hantera projekt, rapportering, material och mycket
+              mer.
+            </p>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,40 +1,23 @@
 import React from "react";
 
 interface CardProps {
-  title: string;
   colorName: string;
   children: React.ReactNode;
+  className?: string;
 }
 
 const Card = (props: CardProps) => {
-  const { title, children, colorName } = props;
+  const { children, colorName, className } = props;
   //ref for the current card
   const cardRef = React.useRef<HTMLDivElement>(null);
 
   //if the card is positioned at the top of the container, remove the shadow using IntersectionObserver
-  React.useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            cardRef.current?.classList.add("shadow-xl");
-          } else {
-            cardRef.current?.classList.remove("shadow-xl");
-          }
-        });
-      },
-      { threshold: 1 }
-    );
-    observer.observe(cardRef.current!);
-    return () => observer.disconnect();
-  }, []);
 
   return (
     <div
-      className={`flex flex-col mb-5 mx-4 px-6 py-4 ${colorName} sticky top-[-1px] min-h-80 max-w-[700px]`}
+      className={`flex flex-col p-12 ${colorName}  rounded-xl ${className}`}
       ref={cardRef}
     >
-      {title && <h1 className="text-3xl font-extrabold mt-2 mb-3">{title}</h1>}
       {children}
     </div>
   );
