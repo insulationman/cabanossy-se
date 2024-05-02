b40594c1744061ddcc30a65f7b2f867e93466b2a
Adam Liljenberg <adam@cabanossy.se>
2024-05-02 12:37:59 +0200

    Update sticky header and card positioning in App.tsx and Card.tsx

--- src/Card.tsx
+++ src/Card.tsx
@@ -7,9 +7,33 @@ interface CardProps {
 
 const Card = (props: CardProps) => {
   const { title, children } = props;
+  //ref for the current card
+  const cardRef = React.useRef<HTMLDivElement>(null);
+
+  //if the card is positioned at the top of the container, remove the shadow using IntersectionObserver
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            cardRef.current?.classList.add("shadow-xl");
+          } else {
+            cardRef.current?.classList.remove("shadow-xl");
+          }
+        });
+      },
+      { threshold: 1 }
+    );
+    observer.observe(cardRef.current!);
+    return () => observer.disconnect();
+  }, []);
 
   return (
-    <div className="flex flex-col mb-5 mx-4 px-6 py-4 bg-yellow-200 shadow-xl sticky top-0 min-h-80 ">
+    <div
+      className="flex flex-col mb-5 mx-4 px-6 py-4 bg-yellow-200 shadow-xl sticky top-[-1px] min-h-80 "
+      ref={cardRef}
+    >
+      {cardRef.current?.offsetTop}
       {title && <h1 className="text-3xl font-extrabold mt-2 mb-3">{title}</h1>}
       {children}
     </div>
