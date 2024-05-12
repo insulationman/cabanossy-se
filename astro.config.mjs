e222f6e7bb9a0f2ec4ac66cc69a94ff343078adc
Adam Liljenberg <adam@cabanossy.se>
2024-05-12 15:19:50 +0200

    Added auto generated favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import favicons from "astro-favicons"; // Add code manually
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx()],
+  integrations: [
+    tailwind(),
+    icon(),
+    mdx(),
+    favicons({
+      masterPicture: "./src/assets/favicon.svg",
+      emitAssets: true,
+    }),
+  ],
 });
