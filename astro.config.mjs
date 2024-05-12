bea4bc80e4f9078ab841a3c7d7e562aadd54e894
Adam Liljenberg <adam@cabanossy.se>
2024-05-12 15:37:06 +0200

    Manually added favicons and removed package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import favicons from "astro-favicons"; // Add code manually
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    icon(),
-    mdx(),
-    favicons({
-      masterPicture: "./src/assets/favicon.svg",
-      emitAssets: true,
-      faviconsDarkMode: false,
-    }),
-  ],
+  integrations: [tailwind(), icon(), mdx()],
 });
