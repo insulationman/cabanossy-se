76ef06629eb71b5031b1e683ad5f27986a353e65
Adam Liljenberg <adam@cabanossy.se>
2024-05-24 13:24:37 +0200

    Added sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx()],
+  site: "https://cabanossy.se",
+  integrations: [tailwind(), icon(), mdx(), sitemap()],
 });
